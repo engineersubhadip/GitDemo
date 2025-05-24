2f889f66ef9a90e6b5ededeccfe3f103e3eac05e
Subhadip Das <subhadipdas911@gmail.com>
2025-05-24 11:54:20 +0530

    Story 3.1 : User Input

--- src/test/java/StepDefs/File1.java
+++ src/test/java/StepDefs/File1.java
@@ -3,5 +3,6 @@ package StepDefs;
 public class File1 {
     public static void main(String[] args) {
         System.out.println("Hello World !!!");
+        System.out.println("Take input from the user and add them into the account");
     }
 }
