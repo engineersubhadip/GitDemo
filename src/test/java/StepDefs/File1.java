aeb51df83924389ef74066eb1d31b72c62bf2344
Subhadip Das <subhadipdas911@gmail.com>
2025-05-25 10:41:59 +0530

    My sixth commit

--- src/test/java/StepDefs/File1.java
+++ src/test/java/StepDefs/File1.java
@@ -5,6 +5,10 @@ public class File1 {
         System.out.println("Name is : " + name);
     }
 
+    static void sayHello(String name) {
+        System.out.println(name+" says hello !");
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello World !!!");
         System.out.println("Take input from the user and add them into the account");
